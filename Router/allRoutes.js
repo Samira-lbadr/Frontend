445cbe7fc37d96b38a58f04b2659c6ad5e246624
samira-albadr <160395156+samira-albadr@users.noreply.github.com>
2024-02-29 14:58:53 +0100

    pages auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,14 @@
 import Route from "./Route.js";  
 //Définir ici vos routes 
-export const allRoutes = [ 
-    new Route("/", "Accueil", "/pages/home.html"),  
-    new Route("/Galerie", "La galerie", "/pages/galerie.html"), 
-    
-]; 
+export const allRoutes = [  
+    new Route("/", "Accueil", "pages/home.html"), 
+    new Route("/galerie", "La galerie", "pages/galerie.html"),
+    new Route("/signin", "Connexion", "pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "pages/auth/account.html"),
+    new Route("/EditPassword", "Changement de mot de passe", "pages/auth/EditPassword.html"),
+];   
+ 
 //Le titre s'affiche comme ceci : Route.titre - websitename 
 export const websiteName = "Tables Amazighs";
+
